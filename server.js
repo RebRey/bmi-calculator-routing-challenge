41e23789d67831aa455292452f6780c990d3e448
Rebecca Reyes <rebreyaz@gmail.com>
2023-01-19 14:19:22 -0700

    update bmi formula

--- server.js
+++ server.js
@@ -5,17 +5,33 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res) {
-res.sendFile(__dirname + "/bmi-calculator");
+// Calculator App
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function (req, res){
+app.post("/", function (req, res) {
+    // number converts the text input to numbers
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 + num2;
+    res.send("The result of the calculation is " + result);
 });
+
+app.listen(port, function () {
+    console.log("Sever started successfully on port 3000.");
+  });
+
+// BMI Calculator App
 
 app.get("/bmi-calculator", function(req,res){
-    res.sendFile(__dirname + "/bmi-calculator");
+    res.sendFile(__dirname + "/bmi-calculator.html");
 });
 
-app.listen(port, function(){
-console.log("Server is running on port 3000.");
+app.post("/bmi-calculator", function(req, res) {
+    // parseFloat converts the text input into decimal numbers
+var weight = parseFloat(req.body.weight);
+var height = parseFloat(req.body.height);
+var bmi = (weight / (Math.pow(height, 2) / 10000)).toFixed(2);
+res.send("Your BMI is " + bmi +".");
 });
